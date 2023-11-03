990644154ac3a6e50a8e51691a5756ecce1b4b2a
abdulbiztech <abdulbiztech@gmail.com>
2023-11-03 13:00:27 +0530

    first commit

---
+++ variables.js
@@ -0,0 +1,9 @@
+// 3 types of variable in javascript
+const productId = 101;
+let productName = "Abdul";
+var productCity = "Mumbai";
+
+// const is use for declaring varible but we cant change the value
+// let variable we use to define the variable and we can change it
+// var variable dont use because of isse in block scope and functional component
+console.table([productId, productName, productCity]);
