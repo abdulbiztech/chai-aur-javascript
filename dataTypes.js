f0eb6f72edf10a4af32721c6f48c703796d425be
abdulbiztech <abdulbiztech@gmail.com>
2023-11-04 18:22:26 +0530

    data types

---
+++ dataTypes.js
@@ -0,0 +1,17 @@
+let name = "Abdulkarim";
+let age = false;
+let isLoggedIn = true;
+
+let convert = Number(age);
+console.log("convert", typeof convert);
+console.log(convert);
+
+// Note
+//"33"=>33
+// "33abc"=>NaN
+//true => 1;false=>0
+
+// Interview Questions
+// Types of dataTypes
+// 1)Primitive(Call by Value) 7 type=> String,Number,Boolean,Null,Undefined,symbol,BigInt
+// 2)Non-Priminitive(Call by Reference) => Array,Object,Functions
--- variables.js
+++ variables.js
@@ -3,7 +3,8 @@ const productId = 101;
 let productName = "Abdul";
 var productCity = "Mumbai";
 
+console.table([productId, productName, productCity]);
+// Note
 // const is use for declaring varible but we cant change the value
 // let variable we use to define the variable and we can change it
 // var variable dont use because of isse in block scope and functional component
-console.table([productId, productName, productCity]);
